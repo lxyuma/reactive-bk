31d668468a4c79c58015c4f41ff0680d9a4a292c
ryuma.tsukano <lyumalyuma@gmail.com>
2014-02-02 13:01:18 +0900

    add event

--- app/scripts/views/tasklist.js
+++ app/scripts/views/tasklist.js
@@ -7,6 +7,12 @@ var ractive = ractive || {};
         template: "#task-list-template",
         init: function(options){
             this.set('taskList', []);
+            this.on({
+                onClickTaskItem: function(e, item){
+                    item["finished"] = true;
+                    this.update("taskList");
+                }
+            });
         }
     });
 })();
