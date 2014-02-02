7f13123c4546208c2daf827ca27ebd190fe1eee7
ryuma.tsukano <lyumalyuma@gmail.com>
2014-02-02 10:38:36 +0900

    1st commit

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global ractiveBk, $*/
+
+
+window.ractiveBk = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    ractiveBk.init();
+});
